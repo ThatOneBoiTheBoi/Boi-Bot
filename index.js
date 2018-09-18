f056609493f05e0190193dd443ca111fcc6cb612
ThatOneBoiTheBoi <43328177+ThatOneBoiTheBoi@users.noreply.github.com>
2018-09-18 12:46:57 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-index.js
-
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
 
 client.on('ready', () => {
-
   console.log(`Logged in as ${client.user.tag}!`);
 
 });
 
 
 client.on('message', msg => {
-
   if (msg.content === 'hi') {
 
     msg.reply('hi!');
