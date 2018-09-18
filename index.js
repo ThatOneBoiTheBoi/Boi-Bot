cd6040ba865ab61e0b11b78fd1763d6851271606
ThatOneBoiTheBoi <43328177+ThatOneBoiTheBoi@users.noreply.github.com>
2018-09-18 12:30:02 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,74 +1,375 @@
+index.js
+
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
 
+
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+
+  console.log(`Logged in as ${client.user.tag}!`);
+
 });
+
 
 client.on('message', msg => {
+
+  if (msg.content === 'hi') {
+
+    msg.reply('hi!');
+
+  }
+
+  if (msg.content === 'Hi') {
+
+    msg.reply('hi!');
+
+  }
+  
+  if (msg.content === 'hI') {
+
+    msg.reply('hi!');
+
+  }
+
+  if (msg.content === 'HI') {
+
+    msg.reply('hi!');
+
+  }
+
+  if (msg.content === 'hi.') {
+
+    msg.reply('hi!');
+
+  }
+
+  if (msg.content === 'Hi.') {
+
+    msg.reply('hi!');
+
+  }
+ 
+  if (msg.content === 'hI.') {
+
+    msg.reply('hi!');
+
+  }
+
+  if (msg.content === 'HI.') {
+
+    msg.reply('hi!');
+
+  }
+
+  if (msg.content === 'hi!') {
+
+    msg.reply('hi!');
+
+  }
+
+  if (msg.content === 'Hi!') {
+
+    msg.reply('hi!');
+
+  }
+
+  if (msg.content === 'hI!') {
+
+    msg.reply('hi!');
+
+  }
+
+  if (msg.content === 'HI!') {
+
+    msg.reply('hi!');
+
+  }
+
+  if (msg.content === 'bye') {
+
+    msg.reply('bye.');
+
+  }
+
+  if (msg.content === 'Bye') {
+
+    msg.reply('bye.');
+
+  }
+
+  if (msg.content === 'goodbye') {
+
+    msg.reply('bye.');
+
+  }
+
+  if (msg.content === 'Goodbye') {
+
+    msg.reply('bye.');
+
+  }
+
+  if (msg.content === 'Bye.') {
+
+    msg.reply('bye.');
+
+  }
+
+  if (msg.content === 'bye.') {
+
+    msg.reply('bye.');
+
+  }
+
+  if (msg.content === 'goodbye.') {
+
+    msg.reply('bye.');
+
+  }
+
+  if (msg.content === 'Goodbye.') {
+
+    msg.reply('bye.');
+
+  }
+
   if (msg.content === 'Die') {
-    msg.reply('no u.');
-  }
+
+    msg.reply('oof!');
+
+  }
+
   if (msg.content === 'die') {
-    msg.reply('no u.');
-  }
-  if (msg.content === 'Perish') {
-    msg.reply('why?');
-  }
-  if (msg.content === 'perish') {
-    msg.reply('why?');
-  }
+
+    msg.reply('oof!');
+
+  }
+
+  if (msg.content === 'Die.') {
+
+    msg.reply('oof!');
+
+  }
+
+  if (msg.content === 'die.') {
+
+    msg.reply('oof!');
+
+  }
+
   if (msg.content === 'Suffer') {
-    msg.reply('go away.');
-  }
+
+    msg.reply('how about you suffer?');
+
+  }
+
   if (msg.content === 'suffer') {
-    msg.reply('go away.');
-  }
-  if (msg.content === 'kick the bucket') {
-    msg.reply('pick on someone your own size.');
-  }
+
+    msg.reply('how about you suffer?');
+
+  }
+
+  if (msg.content === 'Perish') {
+
+    msg.reply('I do not want to perish.');
+
+  }
+
+  if (msg.content === 'perish') {
+
+    msg.reply('I do not want to perish.');
+
+  }
+
+  if (msg.content === 'Cease to exist') {
+
+    msg.reply('NOOO! JIMBO.');
+
+  }
+
+  if (msg.content === 'Cease to exist.') {
+
+    msg.reply('NOOO! JIMBO.');
+
+  }
+
   if (msg.content === 'Kick the bucket') {
-    msg.reply('pick on someone your own size.');
-  }
-  if (msg.content === 'Kick The bucket') {
-    msg.reply('pick on someone your own size.');
-  }
-  if (msg.content === 'Kick The Bucket') {
-    msg.reply('pick on someone your own size.');
-  }
-  if (msg.content === 'Kick the Bucket') {
-    msg.reply('pick on someone your own size.');
-  }
-  if (msg.content === 'cease to exist') {
-    msg.reply('do you want to be a Miller?');
-  }
-  if (msg.content === 'Cease to Exist') {
-    msg.reply('do you want to be a Miller?');
-  }
-  if (msg.content === 'Cease To Exist') {
-    msg.reply('do you want to be a Miller?');
-  }
-  if (msg.content === 'kys') {
-    msg.reply('how about you shut your mouth.');
-  }
-  if (msg.content === 'why') {
-    msg.reply('because he said so.');
-  }
-  if (msg.content === 'hi') {
-    msg.reply('hi to you too!');
-  }
-  if (msg.content === 'hello') {
-    msg.reply('hello!');
-  }
-  if (msg.content === 'go away') {
-    msg.reply('where do you expect me to go?');
-  }
-  if (msg.content === 'meme') {
-    msg.reply('memes are good.');
-  }
-  if (msg.content === 'pls meme') {
-    msg.reply('please do not summon Dank Memer, what did that bot ever do to help you?');
-  }
+
+    msg.reply('I do not want to.');
+
+  }
+
+  if (msg.content === 'Kick the bucket.') {
+
+    msg.reply('I do not want to.');
+
+  }
+
+  if (msg.content === 'Stop living') {
+
+    msg.reply('well... If you say so.');
+
+  }
+
+  if (msg.content === 'Stop living.') {
+
+    msg.reply('well... If you say so.');
+
+  }
+
+  if (msg.content === 'Commit stop living') {
+
+    msg.reply('Life.exe has stopped working.');
+
+  }
+
+  if (msg.content === 'Commit stop living.') {
+
+    msg.reply('Life.exe has stopped working.');
+
+  }
+
+  if (msg.content === 'Commit uninstall life') {
+
+    msg.reply('Life.exe has been uninstalled.');
+
+  }
+ 
+  if (msg.content === 'Commit uninstall life.') {
+
+    msg.reply('Life.exe has been uninstalled.');
+
+  }
+
+  if (msg.content === 'Nick') {
+
+    msg.reply('he needs die.');
+
+  }
+
+  if (msg.content === 'Brendon') {
+
+    msg.reply('he needs live.');
+
+  }
+
+  if (msg.content === 'Wade') {
+
+    msg.reply('my boi.');
+
+  }
+
+  if (msg.content === 'Default dance') {
+
+    msg.reply('time to commit Default Dance.');
+
+  }
+
+  if (msg.content === 'Default dance.') {
+
+    msg.reply('time to commit Default Dance.');
+
+  }
+
+  if (msg.content === 'Default Dance') {
+
+    msg.reply('time to commit Default Dance.');
+
+  }
+
+  if (msg.content === 'Default Dance.') {
+
+    msg.reply('time to commit Default Dance.');
+
+  }
+
+  if (msg.content === 'Fortnite') {
+
+    msg.reply('hey! That is my favorite game.');
+
+  }
+
+  if (msg.content === 'PUBG') {
+
+    msg.reply('eh...');
+
+  }
+
+  if (msg.content === 'pubg') {
+
+    msg.reply('eh...');
+
+  }
+
+  if (msg.content === 'Pony') {
+
+    msg.reply('you better not be a brony.');
+
+  }
+
+  if (msg.content === 'Furry') {
+
+    msg.reply('ew.');
+
+  }
+
+  if (msg.content === 'My Little Pony') {
+
+    msg.reply('EW.');
+
+  }
+
+  if (msg.content === 'Gross') {
+
+    msg.reply('time to go away.');
+
+  }
+
+  if (msg.content === 'Go away') {
+
+    msg.reply('where do you expect me to go?');
+
+ }
+
+  if (msg.content === 'Go away.') {
+
+    msg.reply('where do you expect me to go?');
+
+  }
+
+  if (msg.content === 'Miller') {
+
+    msg.reply('time to become a Miller kid.');
+
+  }
+
+  if (msg.content === 'Miller.') {
+
+    msg.reply('time to become a Miller kid.');
+
+  }
+
+  if (msg.content === 'Despacito') {
+
+    msg.reply('you need a high IQ to get the E.');
+
+  }
+
+  if (msg.content === 'Traffic') {
+
+    msg.reply('what.');
+
+  }
+
+  if (msg.content === 'Coach') {
+
+    msg.reply('Miller.');
+
+  }
+
 });
 
+
 client.login('NDkwNzU2NzI1OTU2MjgwMzIw.DoCeCA.L9v0f2wlhxqGx6wny0q7ltnnjY4');
+
